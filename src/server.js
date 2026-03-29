739c96ea2df555b602af4fa0018cfdb5c3e0a476
Vladyslav Bobro <vlad.bobro.02@gmail.com>
2026-03-29 22:08:26 +0300

    done

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { connectMongoDB } from './db/connectMongoDb.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
