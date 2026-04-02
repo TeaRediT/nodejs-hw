6c327bce60eb23e83749c3255c3132dfebb456eb
TeaRediT <vlad.bobro.02@gmail.com>
2026-04-02 17:16:09 +0300

    done

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -6,24 +7,22 @@ const noteSchema = new Schema(
     content: { type: String, default: '', trim: true },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
   {
     timestamps: true,
     versionKey: false,
+  },
+);
+
+noteSchema.index(
+  { title: 'text', content: 'text' },
+  {
+    name: 'NoteTextIndex',
+    weights: { title: 11, content: 10 },
+    default_language: 'english',
   },
 );
 
