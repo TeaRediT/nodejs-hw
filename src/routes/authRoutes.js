0f3fb14e66024d40822473bed88af90d337a1d52
TeaRediT <vlad.bobro.02@gmail.com>
2026-04-04 17:06:46 +0300

    done

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,24 @@
+import Router from 'express';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validations/authValidation.js';
+import { celebrate } from 'celebrate';
+import {
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  registerUser,
+} from '../controllers/authController.js';
+
+const router = Router();
+
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+
+router.post('/auth/refresh', refreshUserSession);
+
+router.post('/auth/logout', logoutUser);
+
+export default router;
